cb2e8224b99657a42ddf2e9c61fd536ef93cdcf1
imac <1>
2016-10-25 00:23:17 +0800

    添加脚本

---
+++ apktool.sh
@@ -0,0 +1,22 @@
+ #!/bin/sh
+
+rm -r out_xml
+mkdir dex2jar_app
+
+echo "正在反编译"
+java -jar apktool_2.1.1.jar d  $1   -o out_xml
+echo  "反编译完成XML"
+cp $1  dex2jar
+unzip -o -d dex2jar_app  $1
+dex2jar.sh  dex2jar_app/*.dex
+
+rm -rf dex2jar_app/*
+mv classes*-dex2jar.jar  dex2jar_app/
+echo "生成JAR成功"
+
+
+
+
+
+
+
