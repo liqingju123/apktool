b3415d6d4be006791ba2a15e234f7e963d695569
imac <1>
2016-10-28 13:37:57 +0800

    去除注释

--- find_resource_by_id.sh
+++ find_resource_by_id.sh
@@ -5,6 +5,6 @@ public_type=`grep -i $num  $public_path`
 res_name=$(echo $public_type | sed 's/\<public type="\(.*\)" name="\(.*\)" id="\(.*\)" \/>/\1/g') #获取XML 文件名称类型
 res_id=$(echo $public_type | sed 's/\<public type="\(.*\)" name="\(.*\)" id="\(.*\)" \/>/\2/g') #获取资源对应的ID 名称
 res_name_value=s.xml #为拼接xml资源文件库
-res_path=`find . -type f -name  $res_name$res_name_value  2>/dev/null`#搜索指定的资源文件中的值
+res_path=`find . -type f -name  $res_name$res_name_value  2>/dev/null` #搜索指定的资源文件中的值
 res_id_value=`grep -i $res_id  $res_path`
 echo $res_id_value  #输出该ID 下的资源
